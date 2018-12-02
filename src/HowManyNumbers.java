e9ee75280db3485d05d9c5fcfabe2c7fe069513c
MeilakhSergey <meylakhsergey@gmail.com>
2018-12-02 16:14:04 +0300

    Start

---
+++ src/HowManyNumbers.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class HowManyNumbers {
+    public static void main(String[] args) {
+        List<Long> result = findAll(10, 3);
+        for (Long item : result) {
+            System.out.print(item + " ");
+        }
+    }
+    public static List<Long> findAll(final int sumDigits, final int numDigits) {
+        // Your code here!!
+        return null;
+    }
+}
